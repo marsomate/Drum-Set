43c4b91e565735ab092cd89ccf7f4bf012e9c09d
Marso Mate <marsomate755@gmail.com>
2023-01-19 14:31:23 +0100

    1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-for(i = 0; i < 7; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+for(i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
 
 function handleClick(){
-    alert("I got clicked");
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
 }
